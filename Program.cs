8926cd4b219b52d3e7b88ef8ffd48cbe640566c6
jobelaraw <joshuaangelodator2006@gmail.com>
2026-02-28 16:06:44 +0800

    progress1

--- Program.cs
+++ Program.cs
@@ -1,45 +1,99 @@
-﻿namespace StoreManagement
+﻿using System;
+using System.Collections.Generic;
+namespace StoreManagement
 {
     internal class Program
     {
+        static string[] branchName = new string[3];
+        static string[] branchLocation = new string[3];
+
+        static List<string> accesslogs = new List<string>();
         static void Main(string[] args)
         {
             Console.WriteLine("STORE BRANCH MANAGER\n");
+            PopulateBranches();
+            bool isRunning = true;
 
-            Console.WriteLine("1. Add a Branch");
-            Console.WriteLine("2. View All Branches");
-            Console.WriteLine("3. Update Branch Information");
-            Console.WriteLine("4. Remove Branch");
-            Console.WriteLine("5. Exit");
+            while (isRunning){
 
-            Console.WriteLine("Enter Choice (1 - 5): ");
-            int choice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("1. Add a Branch");
+                Console.WriteLine("2. View All Branches");
+                Console.WriteLine("3. Update Branch Information");
+                Console.WriteLine("4. Remove Branch");
+                Console.WriteLine("5. Exit");
 
-            switch (choice)
-            {
-                case 1:
+                Console.WriteLine("Enter Choice (1 - 5): ");
+                int choice = Convert.ToInt32(Console.ReadLine());
 
-                case 2:
+                switch (choice)
+                {
+                    case 1:
+                        addBranch();
+                        break;
+                    case 2:
+                        ViewBranch();
+                    case 3:
+                        UpdateBranch();
+                        break;
+                    case 4:
+                        RemoveBranch();
+                        break;
+                    case 5:
+                        isRunning = false;
+                        Console.WriteLine("Exiting the Program...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice. Please choose among the options (1 - 5)");
+                        break;
+                }
+            }
+            static void PopulateBranches() {
+                branchName[0] = "Branch 0: Main Branch";
+                branchLocation[0] = "Manila";
 
-                case 3:
-                    UpdateBranch();
-                case 4:
+                branchName[1] = "Branch 1: North Branch";
+                branchLocation[1] = "Baguio City";
 
-
+                branchName[2] = "Branch 2: South Branch";
+                branchLocation[2] = "Binan City";
             }
 
-            static void UpdateBranch() { 
-            Console.WriteLine("ENTER BRANCH INFORMATION\n");
+             static void addBranch()
+                {
+                    for (int i = 0; i < branchName.Length; i++)
+                    {
+                        if (branchName[i] == null)
+                        {
+                            Console.WriteLine("Enter desired Store Branch Name: ");
+                            branchName[i] = Console.ReadLine();
 
-            Console.WriteLine("Enter Store Branch Name: ");
-            string branchName = Console.ReadLine();
+                            Console.WriteLine("Enter Branch Location: ");
+                            branchLocation[i] = Console.ReadLine();
 
-            Console.WriteLine("Enter Branch Location: ");
-            string branchLoc = Console.ReadLine();
+                            Console.WriteLine("Branch Added Successfully!");
+                            return;
+                        }
+                    }
+                }
+                
+                static void ViewBranch() {
+                    Console.WriteLine("BRANCH iNFORMATION\n");
+                    for (int i = 0; i < branchName.Length; i++) {
+                        if (branchName[i] == null) { 
+                        }
+                    }
+                }
 
-            Console.WriteLine("Enter Branch No.: ");
-            int sales = Convert.ToInt32(Console.ReadLine());
+                static void UpdateBranch() {
+                Console.WriteLine("Enter Branch No.: ");
+                int index = Convert.ToInt32(Console.ReadLine());
             }
-        }
+            static void RemoveBranch() { 
+            Console.WriteLine("Enter Branch No. to Remove: ");
+                int index = Convert.ToInt32(Console.ReadLine());
+
+            }
+        
     }
+}
 }
